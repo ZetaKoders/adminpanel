135010d357223f51c67f03b2f07f078e0831ca40
npcnuno <nfcoa2@iscte-iul.pt>
2023-12-15 13:42:13 +0000

    asa

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,7 @@
 
-import { $, component$, type QRL } from '@builder.io/qwik';
-import { routeLoader$, useNavigate } from '@builder.io/qwik-city';
-import type { InitialValues, SubmitHandler } from '@modular-forms/qwik';
-import { formAction$, useForm, valiForm$ } from '@modular-forms/qwik';
-import { Surreal } from 'surrealdb.js';
-import { email, type Input, minLength, object, string } from 'valibot';
-import type { Aluno } from '~/models/types';
+import { component$} from '@builder.io/qwik';
+import { useNavigate } from '@builder.io/qwik-city';
+
 
 
 
@@ -26,15 +22,6 @@ export default component$(() => {
 							await nav(`/Dashboard/users/`);
 						}}
 					>
-						<svg
-							class="w-5 h-5 mr-2 -ml-1"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								fill-rule="evenodd"
-								d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-								clip-rule="evenodd"></path></svg>
                 go to user table
 						</button>
             <button
@@ -44,15 +31,7 @@ export default component$(() => {
 							await nav(`/Dashboard/EventTable/`);
 						}}
 					>
-						<svg
-							class="w-5 h-5 mr-2 -ml-1"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								fill-rule="evenodd"
-								d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-								clip-rule="evenodd"></path></svg>
+						
                 go to event table
 						</button>
 
