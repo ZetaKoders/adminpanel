6e1a7da55bdc0a0bf90fe783b7557e741d699bd1
Orizuro <116589681+Orizuro@users.noreply.github.com>
2023-12-15 17:29:14 +0000

    Update index.tsx, fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,13 +13,13 @@ export default component$(() => {
   return (
     <div class="overflow-x-auto">
       <h1 class="text-3xl text-black pb-6">this is dashboard</h1>
-      <a class="btn" href="http://transfers.orizuro.eu/Dashboard/users">
+      <a class="btn" href="http://transfer.orizuro.eu/Dashboard/users">
         go to users
     </a>
     <div>
 
     </div>
-    <a class="btn" href="http://transfers.orizuro.eu/Dashboard/EventTable">
+    <a class="btn" href="http://transfer.orizuro.eu/Dashboard/EventTable">
         go to users
     </a>
     
